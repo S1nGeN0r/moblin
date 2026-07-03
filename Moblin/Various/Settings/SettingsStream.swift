7a53905b5d0eeb2e332aef7d180dad2398bd3560
blackVanilla <8012556+blackVanilla@users.noreply.github.com>
2026-07-03 22:03:25 +0300

    Add follower and raid alerts for VK Video Live
    Handle the channel_follow_create and channel_raid_income events from
    the info pubsub channel and show them as toasts and highlighted chat
    messages, like for Twitch:
    - New follower: "<nick> just followed!", also updates the latest
      follower for text widgets.
    - Incoming raid: "<nick> raided with a party of <count>!".
    Add an Alerts section (Chat and Toasts) to the VK Video Live stream
    settings with Follows, Rewards and Raids toggles. Reward redemptions
    now respect the toggles as well: the chat highlight can be disabled
    (the message is then shown as a regular chat message) and a toast can
    be shown.
    Nick colors in alerts use the platform's palette, like in chat.
    Note: the DevAPI does not notify the raiding channel about outgoing
    raids, and there is no endpoint to start a raid, so only incoming
    raids are shown.

--- Moblin/Various/Settings/SettingsStream.swift
+++ Moblin/Various/Settings/SettingsStream.swift
@@ -1206,6 +1206,42 @@ class SettingsKickAlerts: Codable, ObservableObject {
     }
 }
 
+class SettingsVkVideoLiveAlerts: Codable, ObservableObject {
+    @Published var follows: Bool = true
+    @Published var rewards: Bool = true
+    @Published var raids: Bool = true
+
+    init() {}
+
+    enum CodingKeys: CodingKey {
+        case follows
+        case rewards
+        case raids
+    }
+
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(.follows, follows)
+        try container.encode(.rewards, rewards)
+        try container.encode(.raids, raids)
+    }
+
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        follows = container.decode(.follows, Bool.self, true)
+        rewards = container.decode(.rewards, Bool.self, true)
+        raids = container.decode(.raids, Bool.self, true)
+    }
+
+    func clone() -> SettingsVkVideoLiveAlerts {
+        let new = SettingsVkVideoLiveAlerts()
+        new.follows = follows
+        new.rewards = rewards
+        new.raids = raids
+        return new
+    }
+}
+
 class SettingsStream: Codable, Identifiable, Equatable, ObservableObject, Named, @unchecked Sendable {
     static let defaultRealtimeIrlBaseUrl = "https://rtirl.com/api"
     static let defaultResolution: SettingsStreamResolution = .r1920x1080
@@ -1243,6 +1279,8 @@ class SettingsStream: Codable, Identifiable, Equatable, ObservableObject, Named,
     var vkVideoLiveAccessToken: String = ""
     @Published var vkVideoLiveLoggedIn: Bool = false
     @Published var vkVideoLiveSendMessagesTo: Bool = true
+    var vkVideoLiveChatAlerts: SettingsVkVideoLiveAlerts = .init()
+    var vkVideoLiveToastAlerts: SettingsVkVideoLiveAlerts = .init()
     @Published var youTubeAuthState: OIDAuthState?
     var youTubeWantsToBeLoggedIn: Bool = false
     var youTubeNotLoggedInCount: Int = 0
@@ -1349,6 +1387,8 @@ class SettingsStream: Codable, Identifiable, Equatable, ObservableObject, Named,
         case vkVideoLiveAccessToken
         case vkVideoLiveLoggedIn
         case vkVideoLiveSendMessagesTo
+        case vkVideoLiveChatAlerts
+        case vkVideoLiveToastAlerts
         case youTubeVideoId
         case youTubeWantsToBeLoggedIn
         case youTubeNotLoggedInCount
@@ -1450,6 +1490,8 @@ class SettingsStream: Codable, Identifiable, Equatable, ObservableObject, Named,
         try container.encode(.vkVideoLiveAccessToken, vkVideoLiveAccessToken)
         try container.encode(.vkVideoLiveLoggedIn, vkVideoLiveLoggedIn)
         try container.encode(.vkVideoLiveSendMessagesTo, vkVideoLiveSendMessagesTo)
+        try container.encode(.vkVideoLiveChatAlerts, vkVideoLiveChatAlerts)
+        try container.encode(.vkVideoLiveToastAlerts, vkVideoLiveToastAlerts)
         if let encoded = encodeYouTubeAuthState() {
             storeYouTubeAuthStateInKeychain(streamId: id, authState: encoded.base64EncodedString())
         }
@@ -1559,6 +1601,12 @@ class SettingsStream: Codable, Identifiable, Equatable, ObservableObject, Named,
         vkVideoLiveAccessToken = container.decode(.vkVideoLiveAccessToken, String.self, "")
         vkVideoLiveLoggedIn = container.decode(.vkVideoLiveLoggedIn, Bool.self, false)
         vkVideoLiveSendMessagesTo = container.decode(.vkVideoLiveSendMessagesTo, Bool.self, true)
+        vkVideoLiveChatAlerts = container.decode(.vkVideoLiveChatAlerts,
+                                                 SettingsVkVideoLiveAlerts.self,
+                                                 .init())
+        vkVideoLiveToastAlerts = container.decode(.vkVideoLiveToastAlerts,
+                                                  SettingsVkVideoLiveAlerts.self,
+                                                  .init())
         if let encoded = loadYouTubeAuthStateFromKeychain(streamId: id) {
             youTubeAuthState = decodeYouTubeAuthState(encoded: Data(base64Encoded: encoded))
         }
@@ -1689,6 +1737,8 @@ class SettingsStream: Codable, Identifiable, Equatable, ObservableObject, Named,
         new.vkVideoLiveAccessToken = vkVideoLiveAccessToken
         new.vkVideoLiveLoggedIn = vkVideoLiveLoggedIn
         new.vkVideoLiveSendMessagesTo = vkVideoLiveSendMessagesTo
+        new.vkVideoLiveChatAlerts = vkVideoLiveChatAlerts.clone()
+        new.vkVideoLiveToastAlerts = vkVideoLiveToastAlerts.clone()
         if vkVideoLiveLoggedIn {
             storeVkVideoLiveAccessTokenInKeychain(streamId: new.id, accessToken: vkVideoLiveAccessToken)
         }
